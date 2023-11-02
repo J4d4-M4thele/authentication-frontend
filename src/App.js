eaaf6e15c5f1c91923a7d9a4882a06e06c6285cd
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-02 16:22:20 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
-//importing components
-import Login from "./auth/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Register from "./auth/Register";
+import Login from "./auth/Login";
 import Home from "./Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-     <Switch>
-       <Route exact path='/register' component={Register} />
-       <Route exact path='/login' component={Login}/>
-       <Route exact path='/' component={Home}/>
-     </Switch>
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+      </Switch>
     </BrowserRouter>
   );
 };
